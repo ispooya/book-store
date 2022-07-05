fa829a61f95e7b8ea1cc9ffe0219465123b2b517
ispooya <ispooya@gmail.com>
2022-07-05 17:33:06 +0430

    feat(books): pass 'title filter', 'authors filter' tests

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\PostBookRequest;
 use App\Http\Requests\PostBookReviewRequest;
 use App\Http\Resources\BookResource;
 use App\Http\Resources\BookReviewResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +19,28 @@ class BooksController extends Controller
 {
     public function getCollection(Request $request)
     {
-        $books = Book::with(['authors'])->paginate(15);
+        $books = Book::when(
+            // Author filter
+            $request->has('authors'),
+            function ($q) use ($request) {
+                $authors = explode(',', $request->authors);
+                foreach ($authors as $author) {
+                    $q->orWhereHas('authors', function (Builder $q) use ($author) {
+                        $q->where('id', $author);
+                    });
+                }
+            }
+        )->when(
+            // Title filter
+            $request->has('title'),
+            function ($q) use ($request) {
+                $title = $request->title;
+                $q->where('title', 'LIKE', "%$title%");
+            }
+        )->with(['authors'])->paginate(15);
+
+
+
         $books = collect($books);
         $books->put(
             'links',
